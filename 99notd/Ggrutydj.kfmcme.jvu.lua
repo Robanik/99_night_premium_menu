44afbef0f0ead93f0af33654cca9986cecfc756a
Robanik <168307797+Robanik@users.noreply.github.com>
2025-09-22 12:40:15 +0100

    Update Ggrutydj.kfmcme.jvu.lua

--- 99notd/Ggrutydj.kfmcme.jvu.lua
+++ 99notd/Ggrutydj.kfmcme.jvu.lua
@@ -45,7 +45,7 @@ local Window = Rayfield:CreateWindow({
       Subtitle = "Key System",
       Note = "https://discord.gg/yrE3aXgtjH", -- Use this to tell the user how to get a key
       FileName = "99NIGhtYlSc", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"ROBIKISTOP"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
