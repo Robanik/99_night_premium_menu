c05fe20002631c7b42ad58cde550d7196ae2ccb5
Robanik <168307797+Robanik@users.noreply.github.com>
2025-09-22 12:30:19 +0100

    Update Ggrutydj.kfmcme.jvu.lua

--- 99notd/Ggrutydj.kfmcme.jvu.lua
+++ 99notd/Ggrutydj.kfmcme.jvu.lua
@@ -15,29 +15,41 @@ local Workspace = game:GetService("Workspace")
 
 -- Window Setup
 local Window = Rayfield:CreateWindow({
-    Name = "99 Nights Bring Hub",
-    LoadingTitle = "Beta Edition",
-    LoadingSubtitle = "BY ROBANIK",
-    ConfigurationSaving = {
-        Enabled = true,
-        FolderName = "99NightsForest",
-        FileName = "gptr_bring_v7"
-    },
-    Discord = {
-        Enabled = true,
-        Invite = "https://discord.gg/yrE3aXgtjH",
-        RememberJoins = true
-    },
-    KeySystem = true, -- Включаем KeySystem
-    KeySettings = {
-        Title = "99 Nights Auto Tree Farm | KeySystem",
-        Subtitle = "Enter the key to unlock",
-        Note = "Key: ROBIKSTOP",
-        FileName = "TreeFarmKey",
-        SaveKey = true, -- Сохраняет ключ для повторного использования
-        GrabKeyFromSite = false,
-        Key = "ROBIKSTOP" -- Ключ для доступа
-    }
+local Window = Rayfield:CreateWindow({
+   Name = "99 NIGHT IN THE FOREST | BY ROBANIK",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "99 NIGHT IN THE FOREST",
+   LoadingSubtitle = "BY ROBANIK",
+   ShowText = "OPEN", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Ocean", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = ROBANIKSCRIPTS, -- Create a custom folder for your hub/game
+      FileName = "99GHUB1"
+   },
+
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "https://discord.gg/yrE3aXgtjH", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "99 night [SCRIPT]",
+      Subtitle = "Key System",
+      Note = "https://discord.gg/yrE3aXgtjH", -- Use this to tell the user how to get a key
+      FileName = "99NIGhtYlSc", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"ROBIKISTOP"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
 
 -- Variables for Original Home Tab
