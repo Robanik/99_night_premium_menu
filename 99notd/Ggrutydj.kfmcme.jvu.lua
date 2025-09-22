eeb7efac7c1035b5e1e37be8c284eaa4d0f85d54
Robanik <168307797+Robanik@users.noreply.github.com>
2025-09-22 12:43:03 +0100

    Update Ggrutydj.kfmcme.jvu.lua

--- 99notd/Ggrutydj.kfmcme.jvu.lua
+++ 99notd/Ggrutydj.kfmcme.jvu.lua
@@ -34,8 +34,8 @@ local Window = Rayfield:CreateWindow({
    },
 
    Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "https://discord.gg/yrE3aXgtjH", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
 
@@ -45,7 +45,7 @@ local Window = Rayfield:CreateWindow({
       Subtitle = "Key System",
       Note = "https://discord.gg/yrE3aXgtjH", -- Use this to tell the user how to get a key
       FileName = "99NIGhtYlSc", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"ROBIKISTOP"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
